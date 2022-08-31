347a1b162bf1c666e6aeecbfa7436783afd3319f
Renoa <38197580+OKAUEND@users.noreply.github.com>
2022-08-31 20:11:54 +0900

    変更 - prismaを経由してplanetscaleのモデルと通信する処理を、ドキュメントを参考にかく

--- src/prisma/index.ts
+++ src/prisma/index.ts
@@ -1,1 +1,16 @@
 import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+type Request = {
+  method: string;
+};
+
+export const assetHandler = async (request: Request, response) => {
+  const { method } = request;
+
+  switch (method) {
+    case "GET":
+      const response = await prisma.star.findMany();
+  }
+};
